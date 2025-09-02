e2f0cc7d56d98cf71e7eee558c38fc9e53ae6028
GG <dayalo.293473@fairview.sti.edu.ph>
2025-09-02 22:27:28 +0800

    Update header.php

--- header.php
+++ header.php
@@ -6,9 +6,23 @@
 
     <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: '#d63031',
+                    }
+                },
+                fontFamily: {
+                    sans: ['Poppins', 'sans-serif'],
+                },
+            }
+        }
+    </script>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="css/header.css">
+    
 </head>
 <body class="bg-white text-gray-800">
 
